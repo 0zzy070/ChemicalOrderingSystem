e520ac8bbb3dddd4d0f2adb53167699b0782c5ea
Ching-Yang Tan <jack5542856@gmail.com>
2024-08-23 19:11:17 +0930

    【Feat】Add support for multiple CORS origins with different ports.

--- backend/uni_chemicals_ordering_system/chemical-ordering-system/src/main/java/chemical_ordering_system/config/SecurityConfig.java
+++ backend/uni_chemicals_ordering_system/chemical-ordering-system/src/main/java/chemical_ordering_system/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package chemical_ordering_system.config;
 
 import chemical_ordering_system.jwt.AuthEntryPointJwt;
 import chemical_ordering_system.jwt.AuthTokenFilter;
+
 import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,67 +22,85 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import javax.sql.DataSource;
+
+import java.util.Arrays;
 import java.util.Collections;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    @Autowired
-    DataSource dataSource;
+    @Autowired DataSource dataSource;
 
-    @Autowired
-    private AuthEntryPointJwt unauthorizedHandler;
+    @Autowired private AuthEntryPointJwt unauthorizedHandler;
 
     @Bean
-    public AuthTokenFilter authenticationJwtTokenFilter(){
+    public AuthTokenFilter authenticationJwtTokenFilter() {
         return new AuthTokenFilter();
     }
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> {
-            requests.requestMatchers("api/users/login").permitAll().anyRequest().authenticated();
-        });
-        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
-            @Override
-            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                CorsConfiguration config = new CorsConfiguration();
-                //config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://10.14.28.151:3000")); // 允许所有来源
-                config.setAllowedOriginPatterns(Collections.singletonList("http://*:3000"));
-                config.setAllowedMethods(Collections.singletonList("*")); // 允许所有方法
-                config.setAllowCredentials(true); // 允许发送凭证（如 Cookies）
-                config.setAllowedHeaders(Collections.singletonList("*")); // 允许所有请求头
-                config.setMaxAge(3600L); // 预检请求的最大缓存时间
-                return config;
-            }
-        }));
-        http.exceptionHandling(exception->exception.authenticationEntryPoint(unauthorizedHandler));
-        http.headers(headers->headers.frameOptions(frameoptions->frameoptions.sameOrigin()));
-        http.csrf(csrf->csrf.disable());
-        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.authorizeHttpRequests(
+                (requests) -> {
+                    requests.requestMatchers("api/users/login")
+                            .permitAll()
+                            .anyRequest()
+                            .authenticated();
+                });
+        http.sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.cors(
+                corsCustomizer ->
+                        corsCustomizer.configurationSource(
+                                new CorsConfigurationSource() {
+                                    @Override
+                                    public CorsConfiguration getCorsConfiguration(
+                                            HttpServletRequest request) {
+                                        CorsConfiguration config = new CorsConfiguration();
+                                        // config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://10.14.28.151:3000")); // 允许所有来源
+                                        //
+                                        // config.setAllowedOriginPatterns(Collections.singletonList("http://*:3000"));
+                                        config.setAllowedOriginPatterns(
+                                                Arrays.asList("http://*:3000", "http://*:4000"));
+                                        config.setAllowedMethods(
+                                                Collections.singletonList("*")); // 允许所有方法
+                                        config.setAllowCredentials(true); // 允许发送凭证（如 Cookies）
+                                        config.setAllowedHeaders(
+                                                Collections.singletonList("*")); // 允许所有请求头
+                                        config.setMaxAge(3600L); // 预检请求的最大缓存时间
+                                        return config;
+                                    }
+                                }));
+        http.exceptionHandling(
+                exception -> exception.authenticationEntryPoint(unauthorizedHandler));
+        http.headers(headers -> headers.frameOptions(frameoptions -> frameoptions.sameOrigin()));
+        http.csrf(csrf -> csrf.disable());
+        http.addFilterBefore(
+                authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
-        //UserDetails user1= User.withUsername("user1").password(passwordEncoder().encode("password1")).roles("USER").build();
+    public UserDetailsService userDetailsService() {
+        // UserDetails user1=
+        // User.withUsername("user1").password(passwordEncoder().encode("password1")).roles("USER").build();
         // userDetailsManager.createUser(user1);
-        JdbcUserDetailsManager userDetailsManager=new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
         return userDetailsManager;
-
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception{
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder)
+            throws Exception {
         return builder.getAuthenticationManager();
     }
 }
