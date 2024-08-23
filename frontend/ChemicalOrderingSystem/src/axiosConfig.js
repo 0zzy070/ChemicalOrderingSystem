e9779b0173ab610102def7e94d799e33deeb7a44
Ching-Yang Tan <jack5542856@gmail.com>
2024-08-23 19:46:02 +0930

    【Feat】Sync test branch with dev branch and update environment-specific settings.

--- frontend/ChemicalOrderingSystem/src/axiosConfig.js
+++ frontend/ChemicalOrderingSystem/src/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://13.238.27.37:8080";
+axios.defaults.baseURL = "http://13.238.27.37:9080";
 axios.defaults.headers.common["Access-Control-Allow-Origin"] =
-  "http://13.238.27.37:8080";
+    "http://13.238.27.37:9080";
 axios.defaults.withCredentials = true;
 
 export default axios;
