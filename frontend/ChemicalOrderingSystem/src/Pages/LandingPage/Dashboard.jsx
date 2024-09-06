5e634e24478019c759c4fd32a296405c3f69772a
Gavin_G <gzh9523@gmail.com>
2024-09-07 00:01:37 +0930

    add icon and color of the dashboar and finish chemical page

--- frontend/ChemicalOrderingSystem/src/Pages/LandingPage/Dashboard.jsx
+++ frontend/ChemicalOrderingSystem/src/Pages/LandingPage/Dashboard.jsx
@@ -2,7 +2,9 @@ import { useEffect } from "react";
 import NavigationBar from "../../Components/Layouts/NavigationBar";
 import SideBar from "../../Components/Layouts/SideBar";
 import "./Dashboard.css";
-
+import IconPhArchiveBoxFill from "../../Assets/Icon/IconPhArchiveBoxFill.tsx";
+// import IconArrowBackward from "../../Assets/Icon/IconArrowBackward.tsx";
+// import '../../Assets/Icon/IconPhArchiveBoxFill.tsx';
 function Dashboard() {
   useEffect(() => {
     document.title = "Welcome to your Dashborad";
@@ -20,42 +22,65 @@ function Dashboard() {
               <div className="default-dashboard">
                 <div className="title">
                   <h3>Welcome to our System!</h3>
+                  
                 </div>
                 <div className="cards">
-                  <div className="bigCard card">
-                    <h4>Users</h4>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Aperiam rerum nemo modi a exercitationem necessitatibus
+                  <div className="bigCard card slight-gold">
+                    <div className="bIcon-container">
+                      <IconPhArchiveBoxFill className="bIcon"/>  
+                    </div>
+                    <p style={{ fontSize: "25px" }}>
+                      Total Chemicals in Stock
                     </p>
+                    <h4 style={{ fontSize: "45px" }}>250</h4>
                   </div>
                   <div className="smallCards">
-                    <div className="card">
-                      <h4>Users</h4>
-                      <p className="fs-30 mb-2">4006</p>
-                      <p>10.00% (30 days)</p>
+                    <div className="card slight-purple">
+                      <div className="icon-container purple">
+                        <IconPhArchiveBoxFill className="icon"/>  
+                      </div>
+                      <p>
+                        Pending Chemical Requests
+                      </p>
+                      <h4>15</h4>
                     </div>
-                    <div className="card">
-                      <h4>Total chemicals </h4>
-
-                      <p>1000</p>
+                    <div className="card slight-red">
+                      <div className="icon-container red">
+                        <IconPhArchiveBoxFill className="icon"/>  
+                      </div>
+                      <p>
+                        Chemicals Near Expiry
+                      </p>
+                      <h4>20</h4>
                     </div>
-                    <div className="card">
-                      <h4>Approvals</h4>
-                      <p>2 Approvals Needed</p>
+                    <div className="card slight-green">
+                      <div className="icon-container green">
+                        <IconPhArchiveBoxFill className="icon"/>  
+                      </div>
+                      <p>
+                        Storage Locations with No Remaining Capacity
+                      </p>
+                      <h4>3</h4>
                     </div>
-                    <div className="card">
-                      <h4>Pending orders</h4>
-                      <p>20 Orders Pending</p>
+                    <div className="card slight-grey">
+                      <div className="icon-container grey">
+                        <IconPhArchiveBoxFill className="icon"/>  
+                      </div>
+                      <p>
+                        Most Frequently Ordered Chemical
+                      </p>
+                      <h4>Ethanol (10/month)</h4>
                     </div>
                   </div>
                 </div>
-                <div className="card">
-                  <h4>Users</h4>
+                <div className="card slight-pink">
+                  <div className="icon-container pink">
+                      <IconPhArchiveBoxFill className="icon"/>  
+                  </div>
                   <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Aperiam rerum nemo modi a exercitationem necessitatibus
+                    Disposals Completed in the Last 30 Days
                   </p>
+                  <h4>12</h4>
                 </div>
               </div>
             </div>
